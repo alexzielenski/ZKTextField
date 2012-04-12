d0c20059cdf58acd1bb9ef82848556332af04f29
Alex Zielenski <alex@zielenski.net>
2012-04-11 21:05:19 -0400

    Bullets

--- src/ZKTextField.m
+++ src/ZKTextField.m
@@ -5,8 +5,26 @@
 //  Created by Alex Zielenski on 4/11/12.
 //  Copyright (c) 2012 Alex Zielenski. All rights reserved.
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 
+// This is used for secure text fields for generating bullets rather than text for display.
 @interface ZKSecureGlyphGenerator : NSGlyphGenerator
 @end
 
@@ -68,7 +86,7 @@
 		self.backgroundColor   = [NSColor whiteColor];
 		self.drawsBackground   = YES;
 		self.drawsBorder       = YES;
-		self.secure            = NO;
+		self.secure            = YES;
 		self.shouldClipContent = YES;
 		self.shouldShowFocus   = YES;
 		self.string            = @"";
@@ -130,9 +148,21 @@
 	
 	[self drawInteriorWithRect:dirtyRect];
 	
-	if (!self._currentFieldEditor)
-		[self drawTextWithRect:dirtyRect andString:(self.attributedString && self.attributedString.length > 0) ? self.attributedString : self.attributedPlaceholderString];
-
+	if (!self._currentFieldEditor) {
+		NSAttributedString *currentString = (self.attributedString.length > 0) ? self.attributedString : self.attributedPlaceholderString;
+		
+		if (self.isSecure && (self.attributedString.length > 0)) {
+			NSString *bullets = [@"" stringByPaddingToLength:currentString.length 
+												  withString:[NSString stringWithUTF8String:"•"] 
+											 startingAtIndex:0];
+			NSMutableAttributedString *mar = [currentString.mutableCopy autorelease];
+			[mar replaceCharactersInRange:NSMakeRange(0, mar.length) withString:bullets];
+			currentString = mar;
+		}
+		
+		[self drawTextWithRect:dirtyRect andString:currentString];
+	}
+	
 	if (self._currentFieldEditor && self.shouldShowFocus) {
 		NSSetFocusRingStyle(NSFocusRingOnly);
 		[self._currentClippingPath ? self._currentClippingPath : [NSBezierPath bezierPathWithRect:self.bounds] fill];
@@ -387,7 +417,7 @@
 
 - (void)textDidChange:(NSNotification *)note
 {	
-	self._currentFieldEditor.textContainerInset = NSZeroSize;
+	
 }
 
 - (void)textDidEndEditing:(NSNotification *)note
